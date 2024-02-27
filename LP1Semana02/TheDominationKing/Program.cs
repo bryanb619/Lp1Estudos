550b18fafcc45a955a11a813d91df60780ae92fd
Steven Hall <stevenbryanb.work@gmail.com>
2024-02-27 22:26:07 +0000

    added user input read and convetion to int

--- LP1Semana02/TheDominationKing/Program.cs
+++ LP1Semana02/TheDominationKing/Program.cs
@@ -16,21 +16,18 @@ namespace TheDominationKing
             // request provinces
             Console.WriteLine("Enter your provinces!");
 
+            // read province input & convert to int
+            int p = int.Parse(Console.ReadLine()); 
 
             // request duchies
             Console.WriteLine("Enter your duchies!");
 
+            // read duchies input & convert to int
+            int d = int.Parse(Console.ReadLine()); 
 
-            // 
+            // read estate input & convert to int
             Console.WriteLine("Enter your estates");
-
-
-
-
-
-
-
-        
+            int e = int.Parse(Console.ReadLine()); 
         }
     }
 }
